512bedb06e34679484540dc1eb9a1002771e67d0
eve-sama <948832626@qq.com>
2021-06-19 13:11:31 +0800

    feat: 添加了桩点与连接线的案例

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('container') container: ElementRef;
   graph: Graph;
 
+  // 添加节点(构造函数)
   addNode1(): void {
     // 创建节点
     const rect = new Shape.Rect({
@@ -31,6 +32,7 @@ export class AppComponent implements AfterViewInit {
     this.graph.addNode(rect);
   }
 
+  // 添加节点(graph自带)
   addNode2(): void {
     const rect = this.graph.addNode({
       shape: 'rect', // 指定使用何种图形，默认值为 'rect'
@@ -50,9 +52,10 @@ export class AppComponent implements AfterViewInit {
     });
   }
 
+  // 添加节点(HTML)
   addNode3(): void {
     const data = {
-      shape: 'html',
+      shape: 'html', // 必须指定为html
       x: 100,
       y: 250,
       width: 80,
@@ -75,7 +78,12 @@ export class AppComponent implements AfterViewInit {
     this.graph.addCell(cell);
   }
 
-  addEdge(): void {
+  clear(): void {
+    this.graph.clearCells();
+  }
+
+  // 添加边(节点对节点)
+  addEdgeWithNode(): void {
     this.graph.clearCells();
     const node1 = new Shape.Rect({
       x: 100,
@@ -101,68 +109,266 @@ export class AppComponent implements AfterViewInit {
     });
     const edge = new Shape.Edge({
       source: node1,
-      target: node2,
-    })
+      target: node2
+    });
+    setTimeout(() => this.graph.addCell(node1), 500);
+    setTimeout(() => this.graph.addCell(node2), 1000);
+    setTimeout(() => this.graph.addCell(edge), 1500);
+  }
+
+  // 添加桩点(无连接线)
+  addPort(): void {
+    this.graph.clearCells();
+    // 连接节点分组, 定义好入口和出口样式
+    const groups = {
+      in: {
+        position: 'left',
+        attrs: {
+          circle: {
+            r: 5,
+            magnet: true,
+            stroke: '#CCD4E0',
+            strokeWidth: 1,
+            fill: '#fff'
+          }
+        }
+      },
+      out: {
+        position: 'right',
+        attrs: {
+          circle: {
+            r: 5,
+            magnet: true, // 是否允许吸附
+            stroke: '#CCD4E0',
+            strokeWidth: 1,
+            fill: '#fff'
+          }
+        }
+      }
+    };
+    const node1 = new Shape.Rect({
+      x: 100,
+      y: 100,
+      width: 80,
+      height: 40,
+      attrs: {
+        label: {
+          text: 'Hello'
+        }
+      },
+      ports: {
+        groups,
+        items: [{ group: 'out' }]
+      }
+    });
+    console.log(node1, `node1`);
+    const node2 = new Shape.Rect({
+      x: 300,
+      y: 100,
+      width: 80,
+      height: 40,
+      attrs: {
+        label: {
+          text: 'Hello'
+        }
+      },
+      ports: {
+        groups,
+        items: [{ group: 'in' }]
+      }
+    });
+    setTimeout(() => this.graph.addCell(node1), 500);
+    setTimeout(() => this.graph.addCell(node2), 1000);
+  }
+
+  // 添加桩点(有连接线)
+  addPortWithEdge(): void {
+    this.graph.clearCells();
+    // 定义好入口和出口样式
+    const groups = {
+      in: {
+        position: 'left',
+        attrs: {
+          circle: {
+            r: 5,
+            magnet: true,
+            stroke: '#CCD4E0',
+            strokeWidth: 1,
+            fill: '#fff'
+          }
+        }
+      },
+      out: {
+        position: 'right',
+        attrs: {
+          circle: {
+            r: 5,
+            magnet: true, // 是否允许吸附
+            stroke: '#CCD4E0',
+            strokeWidth: 1,
+            fill: '#fff'
+          }
+        }
+      }
+    };
+    const node1 = new Shape.Rect({
+      x: 100,
+      y: 100,
+      width: 80,
+      height: 40,
+      attrs: {
+        label: {
+          text: 'Hello'
+        }
+      },
+      ports: {
+        groups,
+        items: [{ group: 'out' }]
+      }
+    });
+    console.log(node1, `node1`);
+    const node2 = new Shape.Rect({
+      x: 300,
+      y: 100,
+      width: 80,
+      height: 40,
+      attrs: {
+        label: {
+          text: 'Hello'
+        }
+      },
+      ports: {
+        groups,
+        items: [{ group: 'in' }]
+      }
+    });
+    setTimeout(() => this.graph.addCell(node1), 500);
+    setTimeout(() => this.graph.addCell(node2), 1000);
     setTimeout(() => {
-      this.graph.addCell(node1);
-    }, 500);
-    setTimeout(() => {
-      this.graph.addCell(node2);
-    }, 1000);
-    setTimeout(() => {
+      const data = this.graph.toJSON();
+      const node1Data = data.cells[0];
+      const node2Data = data.cells[1];
+      const edgeData = {
+        source: { cell: node1Data.id, port: node1Data.ports.items[0].id },
+        target: { cell: node2Data.id, port: node2Data.ports.items[0].id }
+      };
+      const edge = new Shape.Edge(edgeData);
       this.graph.addCell(edge);
     }, 1500);
   }
 
-  private initGraph(): void {
-    const that = this;
-    this.graph = new Graph({
-      container: that.container.nativeElement,
-      panning: true, // 画布拖拽
-      selecting: true,
-      width: document.body.scrollWidth,
-      height: 400,
-      background: { color: '#f7f8fa' },
-      connecting: {
-        snap: true, // 连线的过程中距离节点或者连接桩 50px 时会触发自动吸附
-        // highlight: true, // 拖动边时，是否高亮显示所有可用的连接桩或节点 如果开启这个选项, 会导致validateConnection在一开始就触发一次
-        allowBlank: false, // 是否允许连接到画布空白位置的点
-        allowLoop: false, // 是否允许创建循环连线，即边的起始节点和终止节点为同一节点
-        allowNode: false, // 是否允许边链接到节点（非节点上的链接桩）
-        allowEdge: false, // 是否允许边链接到另一个边
-        // connector: 'algo-edge',
-        connector: 'rounded',
-        connectionPoint: 'boundary',
-        // 连接线的样式
-        createEdge(): Edge<Edge.Properties> {
-          return new Shape.Edge({
-            router: { name: 'manhattan' }
-          });
-        },
-        // 是否允许创建连接线(连出的时候)
-        // tslint:disable-next-line: typedef
-        validateMagnet({ cell, magnet }): boolean {
-          return true;
-        },
-        // 是否允许创建连接线(连入的时候)
-        // tslint:disable-next-line: typedef
-        validateConnection({
-          edge,
-          edgeView,
-          sourceView,
-          targetView,
-          sourcePort,
-          targetPort,
-          sourceMagnet,
-          targetMagnet,
-          sourceCell,
-          targetCell,
-          type
-        }) {
-          return true;
+  /** 躲避障碍物的连接线 */
+  addPortWithManhattan(): void {
+    this.graph.clearCells();
+    // 定义好入口和出口样式
+    const groups = {
+      in: {
+        position: 'left',
+        attrs: {
+          circle: {
+            r: 5,
+            magnet: true,
+            stroke: '#CCD4E0',
+            strokeWidth: 1,
+            fill: '#fff'
+          }
+        }
+      },
+      out: {
+        position: 'right',
+        attrs: {
+          circle: {
+            r: 5,
+            magnet: true, // 是否允许吸附
+            stroke: '#CCD4E0',
+            strokeWidth: 1,
+            fill: '#fff'
+          }
         }
       }
+    };
+    const node1 = new Shape.Rect({
+      x: 100,
+      y: 100,
+      width: 80,
+      height: 40,
+      attrs: {
+        label: {
+          text: 'Hello'
+        }
+      },
+      ports: {
+        groups,
+        items: [{ group: 'out' }]
+      }
     });
+    console.log(node1, `node1`);
+    const node2 = new Shape.Rect({
+      x: 300,
+      y: 100,
+      width: 80,
+      height: 40,
+      attrs: {
+        label: {
+          text: 'Hello'
+        }
+      },
+      ports: {
+        groups,
+        items: [{ group: 'in' }]
+      }
+    });
+    setTimeout(() => this.graph.addCell(node1), 500);
+    setTimeout(() => this.graph.addCell(node2), 1000);
+    setTimeout(() => {
+      const data = this.graph.toJSON();
+      const node1Data = data.cells[0];
+      const node2Data = data.cells[1];
+      const edgeData = {
+        router: { name: 'manhattan' },
+        source: { cell: node1Data.id, port: node1Data.ports.items[0].id },
+        target: { cell: node2Data.id, port: node2Data.ports.items[0].id }
+      };
+      const edge = new Shape.Edge(edgeData);
+      this.graph.addCell(edge);
+    }, 1500);
+  }
+
+  print(): void {
+    const data = this.graph.toJSON();
+    console.log(data, `画布的结果转化为JSON`);
+  }
+
+  /** x6画布的一些基础属性 */
+  graphBasicConfig = {
+    panning: true, // 画布拖拽
+    selecting: true,
+    width: document.body.scrollWidth,
+    height: 400,
+    background: { color: '#f7f8fa' },
+    connecting: {
+      snap: true, // 连线的过程中距离节点或者连接桩 50px 时会触发自动吸附
+      allowBlank: false, // 是否允许连接到画布空白位置的点
+      allowLoop: false, // 是否允许创建循环连线，即边的起始节点和终止节点为同一节点
+      allowNode: false, // 是否允许边链接到节点（非节点上的链接桩）
+      allowEdge: false, // 是否允许边链接到另一个边
+      connector: 'rounded',
+      connectionPoint: 'boundary'
+    }
+  };
+
+  private initGraph(): void {
+    const graphConfig = {
+      ...this.graphBasicConfig,
+      container: this.container.nativeElement,
+      // 连接线的样式
+      createEdge(): Edge<Edge.Properties> {
+        return new Shape.Edge({
+          router: { name: 'normal' }
+        });
+      }
+    };
+    this.graph = new Graph(graphConfig);
   }
 
   // 必须是在这个钩子中初始化
